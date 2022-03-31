822b724add471f794ae239fd1a5aae79d456666a
nixxalznix <102720780+nixxalznix@users.noreply.github.com>
2022-03-31 12:40:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -682,7 +682,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/NL_Movies'),
-            InlineKeyboardButton('💠 GᏒOUᎮ 💠', url='https://t.me/NL_Movies_2')
+            InlineKeyboardButton('💠 GᏒOUᎮ 💠', url='https://t.me/NL_Movies2')
             ],[      
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
@@ -805,7 +805,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons= [[
-            InlineKeyboardButton('♥️ 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴 ♥️', url='https://github.com/Jonndeb/all-in-one-extra-features-')
+            InlineKeyboardButton('♥ MADE ME ♥️', url='https://t.me/Nixxalz')
             ],[
             InlineKeyboardButton('🏠 𝙷𝙾𝙼𝙴 🏠', callback_data='start'),
             InlineKeyboardButton('🔐 𝙲𝙻𝙾𝚂𝙴 🔐', callback_data='close_data')
